404e44d14d28c5a300110e68f7b050692a5863ad
Kenn <johnkenneth.ornias@gmail.com>
2024-10-19 18:06:20 +0800

    feat: create vue layout for auth

---
+++ resources/js/Layouts/AuthLayout.vue
@@ -0,0 +1,9 @@
+<script setup>
+	
+</script>
+<template>
+	<h1>Auth</h1>
+	<Link :href="route('login')">Login</Link>
+	<Link :href="route('registration')">Registration</Link>
+	<slot></slot>
+</template>
