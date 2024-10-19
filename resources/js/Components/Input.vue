f92523c2b36a70e73cec62462fa2d36c18bc4c69
Kenn <johnkenneth.ornias@gmail.com>
2024-10-19 18:02:36 +0800

    feat: create vue component Input

---
+++ resources/js/Components/Input.vue
@@ -0,0 +1,46 @@
+<script setup>
+const model = defineModel({
+	type: null,
+	required: true
+});
+
+defineProps({
+	label: {
+		type: String,
+		required: true,
+	},
+	type: {
+		type: String,
+		default: 'text'
+	},
+	message: String,
+	min: {
+		type: [Number, String],
+		default: null
+	},
+	max: {
+		type: [Number, String],
+		default: null
+	},
+	autofocus: {
+		type: Boolean,
+		default: false
+	}
+});
+</script>
+
+<template>
+	<div class="mb-4">
+		<label class="form-label">{{ label }}:</label>
+		<input 
+			v-model="model" 
+			:type="type" 
+			:class="{'is-invalid': message}" 
+			:min="type === 'number' ? min : null" 
+			:max="type === 'number' ? max : null"
+			:autofocus="autofocus"
+			class="form-control mb-2"
+		/>
+		<div v-if="message" class="invalid-feedback">{{ message }}</div>
+	</div>
+</template>
