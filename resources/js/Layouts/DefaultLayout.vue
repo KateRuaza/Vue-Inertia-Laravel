edd71e3044e4a5e4fb56c1b225df63d709977159
Kenn <johnkenneth.ornias@gmail.com>
2024-10-19 18:02:18 +0800

    feat: create default layout

---
+++ resources/js/Layouts/DefaultLayout.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+<template>
+	<h1>Default</h1>
+	<Link :href="route('admin/home')">View Admin</Link>
+	<Link :href="route('login')">Login</Link>
+	<Link :href="route('registration')">Registration</Link>
+	<slot></slot>
+</template>
---
+++ resources/js/Pages/Default/LandingPage.vue
@@ -0,0 +1,5 @@
+<script setup>
+</script>
+<template>
+	<h1>Guest</h1>
+</template>
